b2447475842d793eff2ab06b8f605be28a69c169
OMPRATIK <routprateek4@gmail.com>
2024-08-03 00:38:37 +0530

    complete app

---
+++ controllers/auth.js
@@ -0,0 +1,66 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+      token,
+    },
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  // compare password
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+      token,
+    },
+  });
+};
+
+const updateUser = async (req, res) => {
+  const { email, name, lastName, location } = req.body;
+
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const user = await User.findOneAndUpdate(
+    { _id: req.user.userId },
+    { email, name, lastName, location },
+    { new: true }
+  );
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ user: { ...user, token } });
+};
+
+module.exports = {
+  register,
+  login,
+  updateUser,
+};
---
+++ middleware/authentication.js
@@ -0,0 +1,27 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
+
+const auth = async (req, res, next) => {
+  // check header
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // attach the user to the job routes
+    const testUser = payload.userId === "66a53cde1c29ac34c8c8789e";
+    req.user = {
+      userId: payload.userId,
+      testUser,
+    };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+};
+
+module.exports = auth;
