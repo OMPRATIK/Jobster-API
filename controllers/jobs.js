e749ace08af794f9c8be91631aa83cfee0c029a8
OMPRATIK <routprateek4@gmail.com>
2024-08-06 01:04:57 +0530

    complete app

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,8 +1,9 @@
 const Job = require("../models/Job");
-const { StatusCodes } = require("http-status-codes");
+const { StatusCodes, LENGTH_REQUIRED } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 const mongoose = require("mongoose");
 const moment = require("moment");
+
 const getAllJobs = async (req, res) => {
   const { status, jobType, sort, search } = req.query;
 
@@ -122,7 +123,7 @@ const showStats = async (req, res) => {
     { $group: { _id: "$status", count: { $sum: 1 } } },
   ]);
 
-  stats.reduce((acc, curr) => {
+  stats = stats.reduce((acc, curr) => {
     const { _id: title, count } = curr;
     acc[title] = count;
     return acc;
@@ -134,7 +135,37 @@ const showStats = async (req, res) => {
     declined: stats.declined || 0,
   };
 
-  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications: [] });
+  let monthlyApplications = await Job.aggregate([
+    {
+      $match: {
+        createdBy: mongoose.Types.ObjectId.createFromHexString(req.user.userId),
+      },
+    },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 module.exports = {
